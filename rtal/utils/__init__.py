e176a5527102f59a0347e36a06f986dc665a1c2e
Yi Huang <pphuangyi@gmail.com>
2025-03-06 10:29:08 -0500

    Develop: Utility functions (#7)
    * setup.py for packaging
    
    * updated readme file
    
    * update to package import
    
    * add a demo dataset with 4 samples and code inspecting dataset and sanity check
    
    * sanity check
    
    * visualization and demo plots
    
    * dataset API for the ROM dataset
    
    * add the induced point operator transform (IPOT) model
    
    * add utility functions

---
+++ rtal/utils/__init__.py
@@ -0,0 +1,5 @@
+# from .checkpoint_saver import CheckpointSaver
+from .checkpointer import Checkpointer
+from .cumulator import Cumulator
+from .utils import get_lr, count_parameters, get_data_root, flatten_dict
+from .runtime import runtime
