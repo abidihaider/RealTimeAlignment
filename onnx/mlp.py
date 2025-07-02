fb7e9c3dc04b4afe31943b8db17f4f86b5a76eb9
Huang Yi <pphuangyi@gmail.com>
2025-07-02 12:25:31 -0400

    new mlp model with leakyrelu and without normalization

--- onnx/mlp.py
+++ onnx/mlp.py
@@ -1,42 +1,36 @@
 """
 An MLP-based network to predict misalignment from readout
-Rewritten for ONNX export.
 """
-import math
 import torch
 from torch import nn
 
-class SineActivation(nn.Module):
+from rtal.utils.utils import get_activ_layer
+
+
+class CloudNorm(nn.Module):
     """
-    Sine activate with a frequency factor
+    Apply batch or instance normalization to data in 
+    the shape (batch_size, num_points, num_features).
+    In the forward, we switch the last two dimension,
+    apply the norm, and switch back.
     """
-    def __init__(self, factor):
+    def __init__(self, norm, num_features):
         super().__init__()
-        self.factor = factor
+        if norm == 'batch':
+            self.norm = nn.BatchNorm1d(num_features)
+        elif norm == 'instance':
+            self.norm = nn.InstanceNorm1d(num_features)
+        else:
+            raise KeyError(f'Unknown norm type {norm}!')
 
-    def forward(self, data):
-        return torch.sin(self.factor * data)
-
-
-def sine_init(layer):
-    """
-    SIREN's way of initializing a linear layer
-    """
-    with torch.no_grad():
-        if hasattr(layer, 'weight'):
-            num_input = layer.weight.size(-1)
-            vrange = math.sqrt(6 / num_input) / 30
-            layer.weight.uniform_(-vrange, vrange)
-
-
-def first_layer_sine_init(layer):
-    """
-    SIREN's way of initializing the first linear layer
-    """
-    with torch.no_grad():
-        if hasattr(layer, 'weight'):
-            num_input = layer.weight.size(-1)
-            layer.weight.uniform_(-1 / num_input, 1 / num_input)
+    def forward(self, tensor):
+        """
+        tensor: (Batch_size, num_points, num_features)
+        """
+        tensor = tensor.permute(0, 2, 1)
+        tensor = self.norm(tensor)
+        tensor = tensor.permute(0, 2, 1)
+        return tensor
 
 
 class ResLinear(nn.Module):
@@ -48,13 +42,18 @@ class ResLinear(nn.Module):
                  in_features,
                  out_features,
                  rezero,
-                 sine_factor):
+                 activ,
+                 norm):
 
         super().__init__()
 
-        self.norm = nn.LayerNorm(in_features)
+        if norm is None:
+            self.norm_layer = nn.Identity()
+        else:
+            self.norm_layer = CloudNorm(norm, in_features)
+
         self.linear = nn.Linear(in_features, out_features)
-        self.activ = SineActivation(sine_factor)
+        self.activ = get_activ_layer(activ)
 
         if rezero:
             self.re_alpha = nn.Parameter(torch.zeros((1, )))
@@ -62,7 +61,9 @@ class ResLinear(nn.Module):
             self.re_alpha = 1
 
     def forward(self, data):
-        return data + self.re_alpha * self.activ(self.linear(self.norm(data)))
+        residual = data
+        data = self.activ(self.linear(self.norm_layer(data)))
+        return residual + self.re_alpha * data
 
 
 class SubsetSolver(nn.Module):
@@ -71,10 +72,12 @@ class SubsetSolver(nn.Module):
                  subset_size,
                  features,
                  rezero,
-                 sine_factor):
+                 activ,
+                 norm):
 
         super().__init__()
         self.subset_size = subset_size
+
         in_f = in_features * subset_size
 
         layers = []
@@ -83,16 +86,23 @@ class SubsetSolver(nn.Module):
                 layers += [ResLinear(in_features  = in_f,
                                      out_features = out_f,
                                      rezero       = rezero,
-                                     sine_factor  = sine_factor)]
+                                     activ        = activ,
+                                     norm         = norm)]
             else:
-                layers += [nn.LayerNorm(in_f),
+                if norm is None:
+                    norm_layer = nn.Identity()
+                else:
+                    norm_layer = CloudNorm(norm, in_f)
+                layers += [norm_layer,
                            nn.Linear(in_f, out_f),
-                           SineActivation(sine_factor)]
+                           get_activ_layer(activ)]
             in_f = out_f
 
         self.model = nn.Sequential(*layers)
 
-    def __assemble(self, data):
+        self.assemble = self._assemble_shift
+
+    def _assemble_shift(self, data):
         """
         assemble the subset by shifting the input
         """
@@ -101,33 +111,47 @@ class SubsetSolver(nn.Module):
                          dim=-1)
 
     def forward(self, data):
-        subset = self.__assemble(data)
+        subset = self.assemble(data)
+        return self.model(subset)
+
+    def inference(self, data):
+        """
+        Have the option to turn off random permutation
+        for inference.
+        """
+        subset = self._assemble_shift(data)
         return self.model(subset)
 
 
 class MLP(nn.Module):
     """
-    MLP-based network with sine activate to predict misalignment from readout
+    MLP-based network with subset solvers
     """
     def __init__(self,
                  in_features,
                  out_features,
                  embedding_features,
                  subset_config,
-                 sine_factor,
-                 use_sine_init,
-                 rezero):
+                 rezero,
+                 activ,
+                 norm):
 
         super().__init__()
-
         in_f = in_features
 
-        # pointwise embedding
+        # pointwise embedding        
+        if norm is None:
+            norm_layer = nn.Identity()
+        else:
+            norm_layer = CloudNorm(norm, in_f)
+    
         layers = []
         for out_f in embedding_features:
-            layers += [nn.Linear(in_f, out_f),
-                       SineActivation(sine_factor)]
+            layers += [norm_layer,
+                       nn.Linear(in_f, out_f),
+                       get_activ_layer(activ)]
             in_f = out_f
+
         self.embed = nn.Sequential(*layers)
 
         # subset solvers
@@ -136,16 +160,14 @@ class MLP(nn.Module):
             layers += [SubsetSolver(in_features = in_f,
                                     subset_size = config[0],
                                     features    = config[1:],
-                                    sine_factor = sine_factor,
-                                    rezero      = rezero)]
+                                    rezero      = rezero,
+                                    activ       = activ,
+                                    norm        = norm)]
             in_f = config[-1]
         self.solvers = nn.ModuleList(layers)
 
         # output layer
         self.output = nn.Linear(in_f, out_features)
-
-        if use_sine_init:
-            self.initialize()
 
     def forward(self, data):
         """
@@ -156,6 +178,13 @@ class MLP(nn.Module):
             data = solver(data)
         return self.output(data.mean(dim=1))
 
-    def initialize(self):
-        self.apply(sine_init)
-        self.embed[0].apply(first_layer_sine_init)
+    def inference(self, data):
+        """
+        Provide the option to turn of random permutation for inference.
+        Input:
+            data of shape (batch_size, num_entries, in_features)
+        """
+        data = self.embed(data)
+        for _, solver in enumerate(self.solvers):
+            data = solver.inference(data)
+        return self.output(data.mean(dim=1))
